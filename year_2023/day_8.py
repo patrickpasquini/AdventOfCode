7ab6126bd3405df0c30407bd7524a47c7ba70ded
Patrick Pasquini <contatopasquini@gmail.com>
2024-01-08 13:46:55 -0300

    ✨ day_8

--- year_2023/day_8.py
+++ year_2023/day_8.py
@@ -1,31 +1,20 @@
 class Day8:
-    def loop_coord():
-        pass
-
     def solution_pt1(documents: list[str]):
         directions = documents[0]
-        values = []
-        coord_tuples = []
-        ZZZ = False
+        nodes = {}
         for doc in documents[2:]:
-            paranthesis_idx = doc.find("(")
-            equal_idx = doc.find("=")
-            values.append(doc[: equal_idx - 1])
-            coord_tuples.append(tuple(doc[paranthesis_idx:].strip("()").split(", ")))
-        coord_pos = 0 if directions[0] == "L" else 1
-        next_value = coord_tuples[0][coord_pos]
-        loops = 1
-        while not ZZZ:
-            for d in directions:
-                coord_pos = 0 if d == "L" else 1
-                value_idx = values.index(next_value)
-                next_value = coord_tuples[value_idx][coord_pos]
-                if next_value == "ZZZ":
-                    loops += 1
-                    ZZZ = True
-                loops += 1
-            loops += 1
-        return loops
+            node_name, node_values = doc.split(" = ")
+            nodes[node_name] = tuple(node_values.strip("()").split(", "))
+        actual_node = "AAA"
+        direction_index = 0
+        steps = 0
+        while actual_node != "ZZZ":
+            direction = directions[direction_index % len(directions)]
+            actual_node = nodes[actual_node][0 if direction == "L" else 1]
+            direction_index += 1
+            steps += 1
+
+        return steps
 
     def solution_pt2(self):
         pass
